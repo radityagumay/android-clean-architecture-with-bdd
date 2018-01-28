f91e7051c906a7da74fd1bab50e6e1a3dc276d94
radityagumay <raditya.g@go-jek.com>
2018-01-28 22:30:46 +0700

    [Task] Add Cache Screen

--- app/src/main/kotlin/com/radityalabs/moviefinder/data/model/response/MovieDetailResponse.kt
+++ app/src/main/kotlin/com/radityalabs/moviefinder/data/model/response/MovieDetailResponse.kt
@@ -4,57 +4,74 @@ import com.google.gson.annotations.SerializedName
 
 sealed class MovieDetail {
     data class Response(
-            @SerializedName("adult") val adult: Boolean? = null,
-            @SerializedName("backdrop_path") val backdropPath: String? = null,
-            @SerializedName("belongs_to_collection") val belongsToCollection: BelongsToCollection? = null,
-            @SerializedName("budget") val budget: Int? = null,
-            @SerializedName("genres") val genres: List<Genre?>? = null,
-            @SerializedName("homepage") val homepage: String? = null,
-            @SerializedName("id") val id: Int? = null,
-            @SerializedName("imdb_id") val imdbId: String? = null,
-            @SerializedName("original_language") val originalLanguage: String? = null,
-            @SerializedName("original_title") val originalTitle: String? = null,
-            @SerializedName("overview") val overview: String? = null,
-            @SerializedName("popularity") val popularity: Double? = null,
-            @SerializedName("poster_path") val posterPath: String? = null,
-            @SerializedName("production_companies") val productionCompanies: List<ProductionCompany?>? = null,
-            @SerializedName("production_countries") val productionCountries: List<ProductionCountry?>? = null,
-            @SerializedName("release_date") val releaseDate: String? = null,
-            @SerializedName("revenue") val revenue: Int? = null,
-            @SerializedName("runtime") val runtime: Int? = null,
-            @SerializedName("spoken_languages") val spokenLanguages: List<SpokenLanguage?>? = null,
-            @SerializedName("status") val status: String? = null,
-            @SerializedName("tagline") val tagline: String? = null,
-            @SerializedName("title") val title: String? = null,
-            @SerializedName("video") val video: Boolean? = null,
-            @SerializedName("vote_average") val voteAverage: Float? = null,
-            @SerializedName("vote_count") val voteCount: Int? = null
+            @SerializedName("adult") val adult: Boolean?,
+            @SerializedName("backdrop_path") val backdropPath: String?,
+            @SerializedName("belongs_to_collection") val belongsToCollection: BelongsToCollection?,
+            @SerializedName("budget") val budget: Int?,
+            @SerializedName("genres") val genres: List<Genre?>?,
+            @SerializedName("homepage") val homepage: String?,
+            @SerializedName("id") val id: Int?,
+            @SerializedName("imdb_id") val imdbId: String?,
+            @SerializedName("original_language") val originalLanguage: String?,
+            @SerializedName("original_title") val originalTitle: String?,
+            @SerializedName("overview") val overview: String?,
+            @SerializedName("popularity") val popularity: Double?,
+            @SerializedName("poster_path") val posterPath: String?,
+            @SerializedName("production_companies") val productionCompanies: List<ProductionCompany?>?,
+            @SerializedName("production_countries") val productionCountries: List<ProductionCountry?>?,
+            @SerializedName("release_date") val releaseDate: String?,
+            @SerializedName("revenue") val revenue: Int?,
+            @SerializedName("runtime") val runtime: Int?,
+            @SerializedName("spoken_languages") val spokenLanguages: List<SpokenLanguage?>?,
+            @SerializedName("status") val status: String?,
+            @SerializedName("tagline") val tagline: String?,
+            @SerializedName("title") val title: String?,
+            @SerializedName("video") val video: Boolean?,
+            @SerializedName("vote_average") val voteAverage: Float?,
+            @SerializedName("vote_count") val voteCount: Int?,
+            @SerializedName("videos") val videos: Videos?,
+            val appendedGenre: String? = null
     )
 
     data class SpokenLanguage(
-            @SerializedName("iso_639_1") val iso6391: String? = null,
-            @SerializedName("name") val name: String? = null
+            @SerializedName("iso_639_1") val iso6391: String?,
+            @SerializedName("name") val name: String?
     )
 
     data class ProductionCompany(
-            @SerializedName("name") val name: String? = null,
-            @SerializedName("id") val id: Int? = null
+            @SerializedName("name") val name: String?,
+            @SerializedName("id") val id: Int?
     )
 
     data class ProductionCountry(
-            @SerializedName("iso_3166_1") val iso31661: String? = null,
-            @SerializedName("name") val name: String? = null
+            @SerializedName("iso_3166_1") val iso31661: String?,
+            @SerializedName("name") val name: String?
     )
 
     data class Genre(
-            @SerializedName("id") val id: Int? = null,
-            @SerializedName("name") val name: String? = null
+            @SerializedName("id") val id: Int?,
+            @SerializedName("name") val name: String?
+    )
+
+    data class Videos(
+            @SerializedName("results") val results: List<Result?>?
+    )
+
+    data class Result(
+            @SerializedName("id") val id: String?,
+            @SerializedName("iso_639_1") val iso6391: String?,
+            @SerializedName("iso_3166_1") val iso31661: String?,
+            @SerializedName("key") val key: String?,
+            @SerializedName("name") val name: String?,
+            @SerializedName("site") val site: String?,
+            @SerializedName("size") val size: Int?,
+            @SerializedName("type") val type: String?
     )
 
     data class BelongsToCollection(
-            @SerializedName("id") val id: Int? = null,
-            @SerializedName("name") val name: String? = null,
-            @SerializedName("poster_path") val posterPath: String? = null,
-            @SerializedName("backdrop_path") val backdropPath: String? = null
+            @SerializedName("id") val id: Int?,
+            @SerializedName("name") val name: String?,
+            @SerializedName("poster_path") val posterPath: String?,
+            @SerializedName("backdrop_path") val backdropPath: String?
     )
 }
--- app/src/main/kotlin/com/radityalabs/moviefinder/data/network/RestConstant.kt
+++ app/src/main/kotlin/com/radityalabs/moviefinder/data/network/RestConstant.kt
@@ -6,5 +6,5 @@ object RestConstant {
     const val api_key       = "api_key=${BuildConfig.API_KEY}"
 
     const val discover      = BuildConfig.BASE_URL + "discover/movie?sort_by=popularity.desc" + "&" + api_key
-    const val movieDetail   = BuildConfig.BASE_URL + "movie/{movie_id}" + "?" + api_key
+    const val movieDetail   = BuildConfig.BASE_URL + "movie/{movie_id}?append_to_response=videos" + "&" + api_key
 }
